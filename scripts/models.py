c77148245226b1a46c52d7dd19820bf2a80c256b
barisbozkurt <barisbozkurt0@gmail.com>
2018-05-04 11:51:11 +0200

    including other cnn models

--- scripts/models.py
+++ scripts/models.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Keras model definitions
-(one option left for this implementation)
-
-Created on May 15th 2017
 
 @author: Baris Bozkurt
 """
@@ -16,7 +13,7 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD
 
 def loadModel(modelName, input_shape, num_classes):
-    '''Loading one of the Keras models defined below
+    '''Loading one of the Keras models defined below(within this function)
 
     To specify a new model, please consider adding a new
         elif modelName=="..." block and place the specification within that block
@@ -32,7 +29,35 @@ def loadModel(modelName, input_shape, num_classes):
             layers of Keras models
     '''
     model=None
-    if modelName=="uocSeq2":
+    if modelName=="uocSeq0":#Model with single convolutional layer
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=input_shape))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.5))
+        model.add(Flatten())
+        model.add(Dense(32,kernel_regularizer=regularizers.l1(0.0005), activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(num_classes, activation='softmax'))
+        model.compile(loss=keras.losses.categorical_crossentropy,
+                      optimizer=keras.optimizers.Adam(lr=0.0003, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0),
+                      metrics=['accuracy'])#learning rate default: 0.001
+
+    elif modelName=="uocSeq1":#Model with two convolutional layers
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid',activation='relu', input_shape=input_shape))
+        model.add(Conv2D(32, (3, 3), strides=(1, 1), padding='valid', activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.5))
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(16,kernel_regularizer=regularizers.l1(0.0002)))
+        model.add(Dense(num_classes, activation='softmax'))
+        model.compile(loss=keras.losses.categorical_crossentropy,
+                      optimizer=keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0),
+                      metrics=['accuracy'])#learning rate default: 0.001
+
+    elif modelName=="uocSeq2":
         model = Sequential()#Model with 4 convolutional layers
         model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=input_shape))
         model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu'))
@@ -45,11 +70,11 @@ def loadModel(modelName, input_shape, num_classes):
         model.add(Flatten())
         model.add(Dense(32,kernel_regularizer=regularizers.l1(0.0001), activation='relu'))
         model.add(Dropout(0.5))
+        model.add(Dense(16,kernel_regularizer=regularizers.l1(0.0001), activation='relu'))
+        model.add(Dropout(0.5))
         model.add(Dense(num_classes, activation='softmax'))
 
         sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
         model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=sgd)
-    else:
-        print('Error: model: '+modelName+' unknown')
 
     return model
